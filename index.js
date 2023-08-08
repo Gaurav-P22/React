fbff3082f4ab71773ab3b5a0c314ba923248831e
Gaurav Prajapati <118986007+Gaurav-P22@users.noreply.github.com>
2023-08-08 17:26:54 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Name,{Name1,f1,f2} from './App';
+
+
+
+
+
+
+ReactDOM.render(
+    <>
+        <ol>
+            <li> First Name</li>
+            <li>{Name}</li>
+            <li>{Name1}</li>
+            <li>{f1()}</li>
+            <li>{f2}</li>
+        </ol>
+    </>
+    ,document.getElementById('root')
+);
