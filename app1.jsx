be606839787943ed2dce2d99c0cca29da7e0bf04
Gaurav Prajapati <118986007+Gaurav-P22@users.noreply.github.com>
2023-08-09 15:50:16 +0530

    Create app1.jsx

---
+++ app1.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Name="Gaurav";
+const Name1="Prince";
+
+function f1()
+{
+    let val1="Hritik";
+    return  val1;
+}
+function f2()
+{
+    let val2="Hritik Srivas";
+    return  val2;
+}
+
+
+
+export default Name;
+export {Name1,f1,f2};
